7cfd35cec87cdd03ec030094e682d814c7821a6d
okarwasiecki <okarwasiecki@gmail.com>
2025-03-04 13:03:26 -0800

    Test - changed the threshold to 125

--- src/Filters/FixedThresholdFilter.java
+++ src/Filters/FixedThresholdFilter.java
@@ -7,7 +7,7 @@ public class FixedThresholdFilter implements PixelFilter {
     private int threshold;
 
     public FixedThresholdFilter() {
-        threshold = 127;
+        threshold = 125;
     }
 
     @Override
